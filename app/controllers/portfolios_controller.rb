d8fbb33106b206b2fd814aa8796f2c9cd6951d51
Adrian Campanaro <adrianca@tpg.com.au>
2018-05-08 23:06:18 +1000

    Added add and edit for  potrfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -5,6 +5,7 @@ class PortfoliosController < ApplicationController
 
 	def new
 		@portfolio_item = Portfolio.new
+		### create something from scratch
 	end
 
   	# POST /portfolios
@@ -21,6 +22,23 @@ class PortfoliosController < ApplicationController
       	else
         	format.html { render :new }
       	end
+    end
+  end
+
+  ## edit_portfolio GET    /portfolios/:id/edit(.:format) portfolios#edit
+  def edit
+  	@portfolio_item = Portfolio.find(params[:id])
+  end
+
+  # PATCH/PUT /portfolios/1
+  def update
+  	@portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
+       else
+        format.html { render :edit }        
+      end
     end
   end
 
