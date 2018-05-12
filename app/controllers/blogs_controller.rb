41d0e29db8d2e0ff3ce19e6c8e1dd55b46b76764
Adrian Campanaro <adrianca@tpg.com.au>
2018-05-13 00:49:59 +1000

    Updated statis for enum

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -43,11 +43,9 @@ class BlogsController < ApplicationController
   def update
     respond_to do |format|
       if @blog.update(blog_params)
-        format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
-        format.json { render :show, status: :ok, location: @blog }
+        format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }        
       else
         format.html { render :edit }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
