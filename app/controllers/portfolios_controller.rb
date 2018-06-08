8422a91304f3c1371533194e8aaea72a3b5e4bc1
Adrian Campanaro <adrianca@tpg.com.au>
2018-06-09 01:20:21 +1000

    Refacttroed strong paramters for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -27,7 +27,8 @@ class PortfoliosController < ApplicationController
 
   	# POST /portfolios
   	def create
-    	@portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+    	##@portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+      @portfolio_item = Portfolio.new(portfolio_params)
     	### telling form what is allowed to access
     	### these are the parameters allowed to go through the system
     	
@@ -51,7 +52,8 @@ class PortfoliosController < ApplicationController
   def update
   	@portfolio_item = Portfolio.find(params[:id])
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+      #if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
        else
         format.html { render :edit }        
@@ -72,6 +74,16 @@ class PortfoliosController < ApplicationController
     respond_to do |format|
       format.html { redirect_to 	portfolios_path, notice: 'Record was destroyed.' }
      end
+  end
+
+ private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title,
+                                      :subtitle,
+                                      :body,
+                                      technologies_attributes: [:name]
+                                     )
   end
 
 end
